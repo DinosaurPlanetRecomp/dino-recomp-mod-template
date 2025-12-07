3a8d910af0737132d9f0b874111478d965351b81
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-12-06 20:37:27 -0500

    Update for recomp v0.2.0

--- src/mod.c
+++ src/mod.c
@@ -1,6 +1,14 @@
 #include "modding.h"
 #include "recomputils.h"
+#include "recompevents.h"
 
-RECOMP_HOOK_RETURN("game_init") void my_game_init_hook() {
+#include "sys/print.h"
+
+RECOMP_HOOK_RETURN("game_init") void my_game_init_hook(void) {
     recomp_printf("Hello World!\n");
+}
+
+RECOMP_ON_GAME_TICK_CALLBACK void my_game_tick_callback(void) {
+    // TIP: Enable "In-Game Debug Interface" in the recomp Debug settings menu to see this!
+    diPrintf("Hello from game_tick!");
 }
